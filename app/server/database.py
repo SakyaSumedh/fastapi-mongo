9e6fdcaad8081f326eacdce7356cbf8e30bd6308
Sumedh Shakya <sumedhshakya11@gmail.com>
2020-09-25 16:38:07 +0545

    master: Asynchronous CRUD operations

--- app/server/database.py
+++ app/server/database.py
@@ -1,4 +1,5 @@
 import motor.motor_asyncio as mongo_client
+from bson.objectid import ObjectId
 
 MONGO_DETAILS = "mongodb://root:p%40ssw0rd@localhost:27017/?authSource=admin"
 
@@ -18,7 +19,6 @@ When the first I/O operation is made, both the database and collection will be c
 
 
 # helpers
-
 def student_helper(student) -> dict:
     return {
         "id": str(student["_id"]),
@@ -28,3 +28,47 @@ def student_helper(student) -> dict:
         "year": student["year"],
         "GPA": student["gpa"],
     }
+
+
+# List all students
+async def retrive_students():
+    students = []
+    async for student in student_collection.find():
+        students.append(student_helper(student)
+    return students
+
+
+# Add a new student
+async def add_student(student_data: dict) -> dict:
+    student = await student_collection.insert_one(student_data)
+    new_student = await student_collection.find_one({"_id": student.inserted_id})
+    return student_helper(new_student)
+
+
+# Retrieve a student with a matching ID
+async def retrive_student(id: str) -> dict:
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        return student_helper(student)
+
+
+# Update a student with a matching ID
+async def update_student(id: str, data: dict):
+    if len(data) < 1:
+        return False
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        updated_student = await student_collection.update_one(
+            {"_id": ObjectId(id)}, {"$set": data}
+        )
+        if updated_student:
+            return True
+        return False
+
+
+# Delete a student
+async def delete_student(id: str):
+    student = await student_collection.find_one({"_id": ObjectId(id)})
+    if student:
+        await student_collection.delete_one({"_id": ObjectId(id)})
+        return True
